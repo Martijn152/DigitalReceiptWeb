2ef046d21ee5047bce5673616fe54230f4a9da9d
Martijn152 <m.dered@hotmail.com>
2020-03-04 10:40:12 +0100

    Added composer library manager
    - Added composer dependency
    - Placed some code in about.php

--- about.php
+++ about.php
@@ -5,6 +5,20 @@ include "view/header.php";
 
 
 
+<?php
+require __DIR__.'/vendor/autoload.php';
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+// This assumes that you have placed the Firebase credentials in the same directory
+// as this PHP file.
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__ . '/digital-receipt-a6570-firebase-adminsdk-ohqny-d2c73c72f0.json');
+$firebase = (new Factory)
+   ->withServiceAccount($serviceAccount)
+   ->withDatabaseUri('https://my-project.firebaseio.com')
+   ->create();
+$database = $firebase->getDatabase();
+var_dump($database);
+?>
 
 
 
